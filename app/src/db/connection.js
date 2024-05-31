efeaaa4df618fbbf4fc2b14466f16d71dfade7a2
nullisdefined <jaeuu.dev@gmail.com>
2024-05-31 16:44:29 +0900

    port 속성 추가

--- app/src/db/connection.js
+++ app/src/db/connection.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: '127.0.0.1',
+    port: '3306',
     user: 'root',
     password: 'root',
     database: 'Bookie',
