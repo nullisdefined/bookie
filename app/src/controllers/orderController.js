a13b1ea807492bd29343c4bca29e808de7f9e520
nullisdefined <jaeuu.dev@gmail.com>
2024-06-03 20:14:21 +0900

    ✨ 주문목록 조회, 상세주문목록 조회 추가

--- app/src/controllers/orderController.js
+++ app/src/controllers/orderController.js
@@ -10,6 +10,7 @@ exports.orderHandler = async(req, res) => {
     let order_id;
     try {
         let params = [delivery.address, delivery.receiver, delivery.contact];
+        let placeholders;
         let [rows] = await connection.execute(
             `INSERT INTO delivery (address, receiver, contact) VALUES (?, ? ,?)`
             , params
@@ -24,10 +25,16 @@ exports.orderHandler = async(req, res) => {
         order_id = rows.insertId;
 
         const orderedBookParams = items.map((item) => [order_id, item.book_id, item.quantity]);
-        const placeholders = orderedBookParams.map(() => '(?, ?, ?)').join(', ');
+        const cartItemIdParams = items.map((item) => item.cartItemId);
+        placeholders = orderedBookParams.map(() => '(?, ?, ?)').join(',');
         await connection.execute(
             `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ${placeholders}`,
             orderedBookParams.flat()
+        );
+        placeholders = cartItemIdParams.map(() => '?').join(',');
+        await connection.execute(
+            `DELETE FROM cartItems WHERE id IN (${placeholders})`,
+            cartItemIdParams.flat()
         );
         res.json({ msg: '주문 완료' });
     } catch(err) {
@@ -35,9 +42,30 @@ exports.orderHandler = async(req, res) => {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'INTERNAL_SERVER_ERROR' });
     }
 };
-exports.getOrdersHandler = (req, res) => {
+exports.getOrdersHandler = async(req, res) => {
+    try {
+        const [rows] = await connection.execute(`
+        SELECT orders.id AS 'order_id', created_at, address, receiver, contact, book_title, total_quantity, total_price 
+        FROM orders LEFT JOIN delivery ON orders.delivery_id = delivery.id
+        `);
+        res.status(StatusCodes.OK).json(rows);
+    } catch(err) {
+        console.error(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'INTERNAL_SERVER_ERROR' });
+    }
 
 };
-exports.getOrderDetailHandler = (req, res) => {
-
+exports.getOrderDetailHandler = async(req, res) => {
+    try {
+        const { orderId } = req.params;
+        const [rows] = await connection.execute(`
+        SELECT book_id, title, author, price, quantity 
+        FROM orderedBook LEFT JOIN books ON book_id = books.id
+        WHERE order_id = ?`, [orderId]
+        );
+        res.status(StatusCodes.OK).json(rows);
+    } catch(err) {
+        console.error(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'INTERNAL_SERVER_ERROR' });
+    }
 };
